1d715ff1468f7ad82871cc93b2060912a380d43b
Sharando <catlordd@gmail.com>
2023-10-17 08:41:15 +0300

    step

--- game.py
+++ game.py
@@ -49,16 +49,9 @@ while(start):
     p = 'Игрок 1' if player else 'Игрок 2'
     
     # Ввод числа и поиск его координат(x, y)
-    number = input(f'{p} выберите число \n')
-    number = int(number)
+    number = int(input(f'{p} выберите число \n'))
     x,y = 0,0       
     
-    for y1,line in enumerate(dask):
-            for x1, n  in enumerate(line): 
-                if number == n:
-                    x, y = x1, y1
-                    print(x, y)
-    # Функция для преображения таблицы в более читаемый вид (str)
     def print_dask(d):
         
         for y1,line in enumerate(d):
@@ -68,47 +61,70 @@ while(start):
             
                 d[y1][x1] = n
         for line in d:
-            print(line)    
+            print(line)
     
-    # Функция, которая показывает куда можно перемещать число
-    def resolution_move(_x:int,_y:int,d):
+    while True:
         
-        #
-        left_stеp, left_jump = True
-        right_stеp, right_jump = True
-        up_stеp, up_jump = True
-        down_stеp, down_jump = True
+        print_dask(dask)
         
-        #
-        if _y == 0: 
+        for y1,line in enumerate(dask):
+                for x1, n  in enumerate(line): 
+                    if number == n:
+                        x, y = x1, y1
+                        print(x, y)
+
+        # Шаг и прижок
+        left_stеp, left_jump = True, True
+        right_stеp, right_jump = True, True
+        up_stеp, up_jump = True, True
+        down_stеp, down_jump = True, True
+        
+        # Куда можно сделать ход, а куда нельзя
+        if y == 0: 
             left_stеp = False
             left_jump = False
-        if _y == 1: left_jump = False
-        if _y == 7: 
+        if y == 1: left_jump = False
+        if y == 7: 
             right_stеp = False
             right_jump = False
-        if _y == 6: right_jump = False
-        if _x == 0: 
+        if y == 6: right_jump = False
+        if x == 0: 
             up_stеp = False
             up_jump = False
-        if _x == 1: up_jump = False
-        if _x == 7: 
+        if x == 1: up_jump = False
+        if x == 7: 
             down_stеp = False 
             down_jump = False
-        if _x == 6: down_jump = False 
+        if x == 6: down_jump = False
+        
+        vector = int(input(f'{p} выберите направление хода \n'))
+        
+        if vector == 8:
+            if up_stеp:
+                dask[x, y-1] = number
+            elif up_jump:
+                dask[x, y-2] = number
+        elif vector == 2:
+            if down_stеp:
+                dask[x, y+1] = number
+            elif down_jump:
+                dask[x, y+2] = number
+        elif vector == 4:
+            if left_stеp:
+                dask[x-1,y] = number
+            elif left_jump:
+                dask[x-2,y] = number
+        elif vector == 6:
+            if right_stеp:
+                dask[x+1, y] = number
+            elif right_jump:
+                dask[x+2, y] = number
+        elif vector == 5:
+            start = False
     
-    # Обратное преображение таблицы в int 
-    '''for y1,line in enumerate(dask):
-        for x1, n  in enumerate(line): 
-            if number == n:
-                x, y = x1, y1
-                print(x, y)
-            n = int(n)            
-            dask[y1][x1] = n'''
-    
-    print_dask(dask)
+        
     
     
     # 
-    start = False
+        
     
