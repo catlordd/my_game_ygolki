19736e423d52d03c006a827aeed7247902cfaf78
Sharando <catlordd@gmail.com>
2023-10-18 09:31:53 +0300

    vector

--- game.py
+++ game.py
@@ -9,6 +9,25 @@ dask = [[1,3,5,0,0,0,0,0],
         [0,0,0,0,0,14,16,18]]
 
 #
+def print_dask(dd:list):
+    
+    d = []
+    
+    for y1,line in enumerate(dd):
+        d.append([])
+        for x1, n  in enumerate(line): 
+            d[y1].append(n)
+    
+    for y1,line in enumerate(d):
+        for x1, n  in enumerate(line): 
+            n = str(n)
+            n = '0'+n if len(n)==1 else n
+            
+            d[y1][x1] = n
+    for line in d:
+        print(line)
+
+#
 player = True
 
 #
@@ -29,39 +48,31 @@ while(start):
                dask[7][5],dask[7][6],dask[7][7]] 
     
     # Условие победы
-    for number in corner1:
-        if number in win_player2:
-            corner1.remove(number)
+    for num in corner1:
+        if num in win_player2:
+            corner1.remove(num)
             print(corner1)
         if len(corner1) == 0:
             start = False
             print('Player_2 is WIN!!!')
             
-    for number in corner2:
-        if number in win_player1:
-            corner2.remove(number)
+    for num in corner2:
+        if num in win_player1:
+            corner2.remove(num)
             print(corner2)
         if len(corner2) == 0:
             start = False
             print('Player_1 is WIN!!!')
     
+    
     #  
     p = 'Игрок 1' if player else 'Игрок 2'
-    
+    print_dask(dask)
     # Ввод числа и поиск его координат(x, y)
     number = int(input(f'{p} выберите число \n'))
     x,y = 0,0       
     
-    def print_dask(d):
-        
-        for y1,line in enumerate(d):
-            for x1, n  in enumerate(line): 
-                n = str(n)
-                n = '0'+n if len(n)==1 else n
-            
-                d[y1][x1] = n
-        for line in d:
-            print(line)
+    
     
     while True:
         
@@ -72,7 +83,7 @@ while(start):
                     if number == n:
                         x, y = x1, y1
                         print(x, y)
-
+                        
         # Шаг и прижок
         left_stеp, left_jump = True, True
         right_stеp, right_jump = True, True
@@ -80,47 +91,44 @@ while(start):
         down_stеp, down_jump = True, True
         
         # Куда можно сделать ход, а куда нельзя
-        if y == 0: 
-            left_stеp = False
-            left_jump = False
-        if y == 1: left_jump = False
-        if y == 7: 
-            right_stеp = False
-            right_jump = False
-        if y == 6: right_jump = False
-        if x == 0: 
-            up_stеp = False
-            up_jump = False
-        if x == 1: up_jump = False
-        if x == 7: 
-            down_stеp = False 
-            down_jump = False
-        if x == 6: down_jump = False
+        if x == 0 or dask[y][x-1]!=0: left_stеp = False   
+        if x <= 1 or dask[y][x-2]!=0: left_jump = False
+        if x == 7 or dask[y][x+1]!=0: right_stеp = False    
+        if x >= 6 or dask[y][x+2]!=0: right_jump = False
+        
+        if y == 0 or dask[y-1][x]!=0: up_stеp = False     
+        if y >= 1 or dask[y-2][x]!=0: up_jump = False
+        if y == 7 or dask[y+1][x]!=0: down_stеp = False     
+        if y >= 6 or dask[y+2][x]!=0: down_jump = False
         
         vector = int(input(f'{p} выберите направление хода \n'))
         
         if vector == 8:
             if up_stеp:
-                dask[x, y-1] = number
+                dask[y-1][x] = number
             elif up_jump:
-                dask[x, y-2] = number
+                dask[y-2][x] = number
         elif vector == 2:
             if down_stеp:
-                dask[x, y+1] = number
+                dask[y+1][x] = number
             elif down_jump:
-                dask[x, y+2] = number
+                dask[y+2][x] = number
         elif vector == 4:
             if left_stеp:
-                dask[x-1,y] = number
+                dask[y][x-1] = number
             elif left_jump:
-                dask[x-2,y] = number
+                dask[y][x-2] = number
         elif vector == 6:
             if right_stеp:
-                dask[x+1, y] = number
+                dask[y][x+1] = number
             elif right_jump:
-                dask[x+2, y] = number
-        elif vector == 5:
+                dask[y][x+2] = number
+        
+        dask[y][x] = 0
+        
+        if vector == 5:
             start = False
+            
     
         
     
